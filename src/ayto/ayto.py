e01e0464c33edd126b5b9627f3b1abcd220e400a
Dan Turkel <daturkel@gmail.com>
2023-01-17 18:28:45 -0500

    partial scenario feature

--- src/ayto/ayto.py
+++ src/ayto/ayto.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from collections import defaultdict
-import pickle
+import json
 
 import numpy as np
 from numpy.typing import NDArray
@@ -29,8 +29,8 @@ class AYTO:
         self.guys = guys
         self.girls = girls
         self.n = len(self.guys)
-        self.guy_ids, self.girl_ids = self._generate_maps()
-        self._scenarios = self._generate_scenarios()
+        self.guy_ids, self.girl_ids = self._initialize_maps()
+        self._scenarios = self._initialize_scenarios()
         self._initialize_probs()
 
     @property
@@ -99,21 +99,27 @@ class AYTO:
 
         return self.num_scenarios
 
-    def _try_partial_scenario(
-        self, matchup: list[tuple[str, str]]
+    def try_partial_scenario(
+        self,
+        matches: list[tuple[str, str]] | None = None,
+        non_matches: list[tuple[str, str]] | None = None,
     ) -> tuple[int, float, pd.DataFrame]:
-        """EXPERIMENTAL Get results for a hypothetical partial scenario.
+        """Get results for a hypothetical partial scenario.
 
-        For example, if you want to know what happens if Al and Kate are a match and Joe
-        and Cindy are a match, pass `matchup=[("Al", "Kate"), ("Joe", "Cindy")]`. The results
-        are how many scenarios match this partial scenario, the probability of this partial
-        scenario, and the couple probabilities if this partial scenario is true.
+        For example, if you want to see what happens if Al and Kate are a match but Joe
+        and Cindy are *not* a match, pass `matches=[("Al", "Kate")], non_matches=[("Joe",
+        "Cindy")]`. The results are how many scenarios match this partial scenario, the
+        probability of this partial scenario, and the couple probabilities if this partial
+        scenario is true.
 
         Parameters
         ----------
-        matchup
+        matches
             A list of tuples of couples (with the guy first in each pair) that are matches
-            in this partial scenario.
+            in this partial scenario. If `None`, only `non_matches` will be considered.
+        non_matches
+            A list of tuples of couples (with guy first in each pair) that are *not* matches
+            in this partial scenario. If `None`, only `matches` will be considered.
 
         Returns
         -------
@@ -126,13 +132,30 @@ class AYTO:
             The couple probabilities if the partial scenario is true.
 
         """
-        matchup_ints = self._parse_matchup(matchup)
-        idx = self._get_matchup_idx(matchup_ints, len(matchup_ints))
+        if (matches is None) and (non_matches is None):
+            raise ValueError("Either matches, non_matches, or both must be provided.")
+
+        idx = [True] * self.num_scenarios
+
+        # index = index & (all matches are true)
+        if matches:
+            match_ints = self._parse_matchup(matches)
+            match_idx = self._get_matchup_idx(match_ints, len(matches))
+            idx = idx & match_idx
+
+        if non_matches:
+            # for each nonmatch
+            for non_match in non_matches:
+                # index = index & this match is not true
+                non_match_ints = self._parse_matchup([non_match])
+                non_match_idx = self._get_matchup_idx(non_match_ints, 1)
+                idx = idx & ~non_match_idx
+
         scenarios = self._scenarios[idx]
-        num_scenarios = scenarios.shape[0]
+        num_hyp_scenarios = scenarios.shape[0]
         probabilities = self._calculate_probabilities(scenarios)
 
-        return num_scenarios, num_scenarios / self.num_scenarios, probabilities
+        return num_hyp_scenarios, num_hyp_scenarios / self.num_scenarios, probabilities
 
     def calculate_probabilities(self):
         """Update the probabilities for each couple.
@@ -147,7 +170,7 @@ class AYTO:
         """
         self.probabilities = self._calculate_probabilities()
 
-    def _parse_matchup(self, matchup: list[tuple[str, str]]):
+    def _parse_matchup(self, matchup: list[tuple[str, str]]) -> list[int]:
         # ensure all names are known
         for guy, girl in matchup:
             if guy not in self.guy_ids:
@@ -165,23 +188,16 @@ class AYTO:
 
         return matchup_ints
 
-    def _generate_maps(self) -> tuple[dict[str, int], dict[str, int]]:
+    def _initialize_maps(self) -> tuple[dict[str, int], dict[str, int]]:
         guy_ids = {name: id_ for id_, name in enumerate(self.guys)}
         girl_ids = {name: id_ for id_, name in enumerate(self.girls)}
 
         return guy_ids, girl_ids
 
-    def _generate_scenarios(self) -> NDArray:
+    def _initialize_scenarios(self) -> NDArray:
         scenarios = faster_permutations(self.n)
 
         return np.array(scenarios)
-
-    def _get_matchup_idx(self, matchup: list[int], beams: int) -> NDArray:
-        # count number of matches between matchup and each scenario
-        sums = (self._scenarios == matchup).sum(axis=1)
-        # true if number of matches is the number of beams, else false
-        idx = sums == beams
-        return idx
 
     def _initialize_probs(self):
         probs = defaultdict(lambda: defaultdict(float))
@@ -191,6 +207,13 @@ class AYTO:
 
         self.probabilities = pd.DataFrame(probs)
 
+    def _get_matchup_idx(self, matchup: list[int], beams: int) -> NDArray:
+        # count number of matches between matchup and each scenario
+        sums = (self._scenarios == matchup).sum(axis=1)
+        # true if number of matches is the number of beams, else false
+        idx = sums == beams
+        return idx
+
     def _calculate_probabilities(self, scenarios: NDArray | None = None):
         if scenarios is None:
             these_scenarios = self._scenarios
@@ -198,6 +221,10 @@ class AYTO:
             these_scenarios = scenarios
 
         num_scenarios = these_scenarios.shape[0]
+        if num_scenarios == 0:
+            raise ValueError(
+                "Impossible scenario provided. Did you enter contradictory data?"
+            )
 
         probs: defaultdict[str, defaultdict[str, float]] = defaultdict(
             lambda: defaultdict(float)
@@ -226,8 +253,14 @@ class AYTO:
             Filepath to save the class to (e.g. "season_5.pickle")
 
         """
-        with open(path, "wb") as f:
-            pickle.dump(self, f)
+        data = {
+            "guys": self.guys,
+            "girls": self.girls,
+            "scenarios": self._scenarios.tolist(),
+            "probabilities": self.probabilities.values.tolist(),
+        }
+        with open(path, "w") as f:
+            json.dump(data, f)
 
     @classmethod
     def load(cls, path: str) -> AYTO:
@@ -244,5 +277,11 @@ class AYTO:
             An AYTO class loaded from the previously saved results.
 
         """
-        with open(path, "rb") as f:
-            return pickle.load(f)
+        with open(path, "r") as f:
+            data = json.load(f)
+
+        instance = cls(data["guys"], data["girls"])
+        instance._scenarios = np.array(data["scenarios"], dtype=np.uint8, order="F")
+        instance.probabilities.loc[:, :] = data["probabilities"]
+
+        return instance
